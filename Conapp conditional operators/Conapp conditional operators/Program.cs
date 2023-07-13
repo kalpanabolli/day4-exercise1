b6a4b05ae3b5b7b0bdd3330785a045b33587b5bf
kalpanabolli11@gmail.com <kalpanabolli11@gmail.com>
2023-07-13 09:42:03 +0530

    first commit

---
+++ Conapp conditional operators/Conapp conditional operators/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Conapp_conditional_operators
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //(condition) ?true case :false case
+            int num1,num2;
+            Console.WriteLine("Enter First Number");
+            num1=int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter Second Number");
+            num2=int.Parse(Console.ReadLine());
+            var result = num1 > num2 ? "First Number is greater" : "Second Number is greater";
+            Console.WriteLine(result);
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ Conapp enum/Conapp enum/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Conapp_enum
+{
+    internal class Program
+    {
+        enum seasons
+        {
+            winter,
+            summer,
+                rainy,
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Current Season is: " + seasons.rainy);
+            Console.WriteLine("Season Number is: \t"+(int)seasons.rainy);
+            Console.ReadKey();
+        }
+    }
+}
